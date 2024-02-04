56b5a38c0e4a7d464fac25fbbfe2325c2dacc82d
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2024-02-04 07:09:17 +0530

    fix: search Params

--- src/app/api/get_image/route.ts
+++ src/app/api/get_image/route.ts
@@ -1,10 +1,14 @@
+
 import { NextRequest, NextResponse } from "next/server";
 import { NextApiRequest } from "next";
 import { useSearchParams } from 'next/navigation'
 
-export function GET(req: NextApiRequest) {
+export const dynamic = 'force-dynamic';
+export function GET(req: NextRequest) {
     try{
-        const text = req.query['text'];
+        const text = req.nextUrl.searchParams.get('text');
+        
+        
         
         return new NextResponse(
             `
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import { Metadata } from 'next';
 import { ChainTVLData } from './api/get_tvl_data/route';
 
 export default async function Home () {
-  const data: ChainTVLData[] = await fetch(`/api/get_tvl_data`).then((res)=>res.json());
+  const data: ChainTVLData[] = await fetch(`${process.env.API_URL}/api/get_tvl_data`).then((res)=>res.json());
   const tvlHTML = data.map(async (obj)=>{
     try{
-      const image = await fetch(`/get_image?text=${obj.name}:+${obj.amount}`);
+      const image = await fetch(`${process.env.API_URL}/get_image?text=${obj.name}:+${obj.amount}`);
       const TVLhtml = await image.text();
       return {TVLhtml:TVLhtml,obj:obj}
     }

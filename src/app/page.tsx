1e031e75fb8188b7352070aedfb4f993e4dd3b4e
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2024-02-04 09:47:33 +0530

    fix: Deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 import { ChainTVLData } from './api/get_tvl_data/route';
 
 export default async function Home () {
-  const data: ChainTVLData[] = await fetch(`/api/get_tvl_data`).then((res)=>res.json());
+  const data: ChainTVLData[] = await fetch(`${process.env.API_URL}/api/get_tvl_data`).then((res)=>res.json());
   const tvlHTML = data.map(async (obj)=>{
     try{
-      const image = await fetch(`/api/get_image?text=${obj.name}:+${obj.amount}`);
+      const image = await fetch(`${process.env.API_URL}/api/get_image?text=${obj.name}:+${obj.amount}`);
       const TVLhtml = await image.text();
       return {TVLhtml:TVLhtml,obj:obj}
     }
